bd3d164cfdc5d1b1961756bb1524e2324330f8d1
Joshua Ibrom <joshuaibrom@gmail.com>
2023-02-06 17:24:03 -0600

    Finished base project for A2.

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Joshua Ibrom on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
