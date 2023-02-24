c48859f8ab1c0bf86878a9bef519c5d7e4b7b343
Joshua Ibrom <joshuaibrom@gmail.com>
2023-02-23 22:17:03 -0600

    Added checking for game results

--- tic-tac-toe/tic-tac-toe/Models/game.swift
+++ tic-tac-toe/tic-tac-toe/Models/game.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+enum GameResult {
+    case none, win, draw
+}
+
 class TTTGame {
     public var board: [[Int8]] = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
     public var currentPlayer: Int8 = 0
@@ -15,8 +19,58 @@ class TTTGame {
         if (board[row][col] != -1) {
             return
         }
-        print("Updating")
         board[row][col] = currentPlayer
         currentPlayer = (currentPlayer == 0) ? 1 : 0
+        var didWin = checkForWinner()
+        print(didWin)
+    }
+    
+    func checkForWinner() -> GameResult {
+        // Check rows
+        for r in 0..<3 {
+            let leftmostPlayer = board[r][0]
+            if leftmostPlayer == -1 {
+                continue
+            }
+            var didBreak = false
+            for c in 0..<3 {
+                if board[r][c] != leftmostPlayer {
+                    didBreak = true
+                    break
+                }
+            }
+            if !didBreak {
+                return GameResult.win
+            }
+        }
+        // Check cols
+        for c in 0..<3 {
+            let topmostPlayer = board[0][c]
+            if topmostPlayer == -1 {
+                continue
+            }
+            var didBreak = false
+            for r in 0..<3 {
+                if board[c][r] != topmostPlayer {
+                    didBreak = true
+                    break
+                }
+            }
+            if !didBreak {
+                return GameResult.win
+            }
+        }
+        // Check diagonals
+        if board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != -1 {
+            return GameResult.win
+        }
+        if board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != -1 {
+            return GameResult.win
+        }
+        // Check for a draw by checking if any row contains -1 (unplayed tile)
+        if board.map({$0.contains(-1)}) == [false, false, false] {
+            return GameResult.draw
+        }
+        return GameResult.none
     }
 }
