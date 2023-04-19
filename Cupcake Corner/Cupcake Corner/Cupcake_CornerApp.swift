01cc73d822e1314115b09f8f0561b649c89ba780
Joshua Ibrom <joshuaibrom@gmail.com>
2023-04-19 15:39:02 -0500

    A6 Complete

---
+++ Cupcake Corner/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Joshua Ibrom on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
